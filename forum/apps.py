e739d2440d1501f0f7b5984a83e0b7a41eb4843b
Théo Torregrossa <theotor83@gmail.com>
2025-03-16 10:50:35 +0100

    Fix bbcode tags triggering database access during app initialization

--- forum/apps.py
+++ forum/apps.py
@@ -1,10 +1,15 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def register_bbcode_tags(sender, **kwargs):
+    # Import and register BBCode tags here
+    from forum import bbcode_tags
+    bbcode_tags.register_all()
 
 class ForumConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'forum'
 
     def ready(self):
-        # Import your BBCode tags to register them
-        import forum.bbcode_tags
+        # Connect to post_migrate signal instead of importing directly
+        post_migrate.connect(register_bbcode_tags, sender=self)
