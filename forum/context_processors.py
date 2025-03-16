6b8dde0f832c1a3825e8ca2372d87b29f6b7d12e
Théo Torregrossa <theotor83@gmail.com>
2025-03-17 00:00:14 +0100

    Added auto populate database and fixed online count

--- forum/context_processors.py
+++ forum/context_processors.py
@@ -14,12 +14,14 @@ def base_context(request):
             if profile.last_login and timezone.now() - profile.last_login >= timedelta(minutes=15):
                 profile.last_login = now()
                 profile.save(update_fields=["last_login"])
-                intervalle = now() - timedelta(minutes=30)
-                online_count = Profile.objects.filter(last_login__gte=intervalle).count()
-                if UTF.online_record < online_count:
-                    UTF.online_record = online_count
-                    UTF.online_record_date = now()
-                    UTF.save(update_fields=["online_record", "online_record_date"])
+            # This will run every time a user views a page, and will update the online count if the user is online, which is horrible for the database.
+            # We should probably use a signal instead, but this is easier to understand for now.
+            intervalle = now() - timedelta(minutes=30)
+            online_count = Profile.objects.filter(last_login__gte=intervalle).count()
+            if UTF.online_record < online_count:
+                UTF.online_record = online_count
+                UTF.online_record_date = now()
+                UTF.save(update_fields=["online_record", "online_record_date"])
         except Profile.DoesNotExist:
             pass  # In case the user does not have a profile yet
     return {}
