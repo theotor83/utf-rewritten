8eed7560c1bd71434d03f1bb3ce8fb950cc11a4c
Théo Torregrossa <theotor83@gmail.com>
2025-03-25 00:07:17 +0100

    Add more bbcode tags, SCEditor toolbar to all pages

--- forum/bbcode_tags.py
+++ forum/bbcode_tags.py
@@ -17,6 +17,64 @@ class CustomQuoteTag(BBCodeTag):
         '</table>'
     )
 
+# This doesn't work yet but it should
+class CustomQuoteTagUnnamed(BBCodeTag):
+    name = 'quoteunnamed'
+    definition_string = '[quote]{TEXT}[/quote]'
+    format_string = (
+        '<table width="90%" cellspacing="1" cellpadding="3" border="0" align="center">'
+        '   <tbody>'
+        '       <tr>'
+        '           <td><span class="genmed"><b>Citation:</b></span></td>'
+        '       </tr>'
+        '       <tr>'
+        '           <td class="quote">{TEXT}</td>'
+        '       </tr>'
+        '   </tbody>'
+        '</table>'
+    )
+
+class YoutubeTag(BBCodeTag):
+    name = 'youtube'
+    definition_string = '[youtube]{TEXT}[/youtube]'
+    format_string = (
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/{TEXT}" '
+        'frameborder="0" allowfullscreen></iframe>'
+    )
+
+class FontTag(BBCodeTag):
+    name = 'font'
+    definition_string = '[font={TEXT}]{TEXT1}[/font]'
+    format_string = '<span style="font-family: {TEXT} !important;">{TEXT1}</span>'
+
+class SizeTag(BBCodeTag):
+    name = 'size'
+    definition_string = '[size={TEXT}]{TEXT1}[/size]'
+    format_string = '<font size="{TEXT}">{TEXT1}</font>'
+
+# Doesn't work yet because it puts spaces in the URL
+class VideoEmbedTag(BBCodeTag):
+    name = 'video'
+    definition_string = '[video]{TEXT}[/video]'
+    format_string = (
+        '<video width="320" height="240" controls>'
+        '   <source src="{TEXT}" type="video/mp4">'
+        '   Your browser does not support the video tag.'
+        '</video>'
+    )
+
+# class ImageWithResizeTag(BBCodeTag):
+#     name = 'img_resize'
+#     definition_string = '[img={TEXT1}x{TEXT2}]{TEXT}[/img]'
+#     format_string = (
+#         '<img src="{TEXT}" height="{TEXT1}" width="{TEXT2}" />'
+#     )
+
 tag_pool.register_tag(CustomQuoteTag)
+tag_pool.register_tag(CustomQuoteTagUnnamed)
+tag_pool.register_tag(YoutubeTag)
+tag_pool.register_tag(FontTag)
+tag_pool.register_tag(SizeTag)
+tag_pool.register_tag(VideoEmbedTag)
 def register_all():
     print("Hello World")
