40d1b3c9b48eb5986c67ec2fc18ba2231205c820
Théo Torregrossa <152283855+theotor83@users.noreply.github.com>
2025-08-15 14:05:28 +0200

    Add pluralize_0 filter for correct pluralization with zero
    Introduced a new Django template filter pluralize_0 to handle pluralization for both zero and one as singular, improving French language accuracy. Updated all relevant templates to use pluralize_0 instead of pluralize for counts, ensuring correct grammar in user-facing messages.

--- forum/templatetags/templatetags.py
+++ forum/templatetags/templatetags.py
@@ -330,3 +330,44 @@ def get_user_random_color(username):
 @register.filter
 def timestamp(value): # To be used on DateTimeField values in templates
     return value.timestamp() if value else None
+
+@register.filter(is_safe=False)
+def pluralize_0(value, arg="s"):
+    """
+    Return a plural suffix if the value is not 1, '1', or an object of
+    length 1. By default, use 's' as the suffix:
+
+    * If value is 0, vote{{ value|pluralize }} display "vote".
+    * If value is 1, vote{{ value|pluralize }} display "vote".
+    * If value is 2, vote{{ value|pluralize }} display "votes".
+
+    If an argument is provided, use that string instead:
+
+    * If value is 0, class{{ value|pluralize:"es" }} display "class".
+    * If value is 1, class{{ value|pluralize:"es" }} display "class".
+    * If value is 2, class{{ value|pluralize:"es" }} display "classes".
+
+    If the provided argument contains a comma, use the text before the comma
+    for the singular case and the text after the comma for the plural case:
+
+    * If value is 0, cand{{ value|pluralize:"y,ies" }} display "candy".
+    * If value is 1, cand{{ value|pluralize:"y,ies" }} display "candy".
+    * If value is 2, cand{{ value|pluralize:"y,ies" }} display "candies".
+    """
+    if "," not in arg:
+        arg = "," + arg
+    bits = arg.split(",")
+    if len(bits) > 2:
+        return ""
+    singular_suffix, plural_suffix = bits[:2]
+
+    try:
+        return singular_suffix if float(value) == 1 or float(value) == 0 else plural_suffix
+    except ValueError:  # Invalid string that's not a number.
+        pass
+    except TypeError:  # Value isn't a string or a number; maybe it's a list?
+        try:
+            return singular_suffix if len(value) == 1 or len(value) == 0 else plural_suffix
+        except TypeError:  # len() of unsized object.
+            pass
+    return ""
