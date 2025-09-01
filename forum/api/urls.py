1c234dbbf083eee6a000327c82439f5bc8da8141
Théo Torregrossa <152283855+theotor83@users.noreply.github.com>
2025-09-01 22:36:52 +0200

    Add API token authentication endpoint
    Introduced a new URL pattern for API token authentication using Django REST Framework's obtain_auth_token view.

--- forum/api/urls.py
+++ forum/api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken import views as token_views
 
 urlpatterns = [
     path('posts_list/', views.posts_list, name='posts-list'),
     path('profile/<int:userid>/', views.profile_details, name='profile-details'),
+    path('api-token-auth/', token_views.obtain_auth_token, name='api-token-auth'),
 ]
