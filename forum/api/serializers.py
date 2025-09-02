7881d52968529ebadcadec292f7a6f87d4c70c24
Théo Torregrossa <152283855+theotor83@users.noreply.github.com>
2025-09-02 11:13:28 +0200

    Conditionnally show email in profile endpoint
    Updated ProfileDetailsSerializer to include the user's email in the serialized output when the profile's email_is_public flag is set.

--- forum/api/serializers.py
+++ forum/api/serializers.py
@@ -3,28 +3,39 @@ from ..models import *
 from django.contrib.auth.models import User
 
 class PostDebugSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Post
         fields = '__all__'
 
 class UserShortSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ['username', 'date_joined']
 
 class GroupSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = ForumGroup
         fields = ['name', 'description', 'color', 'priority', 'is_staff_group', 
                   'is_messages_group', 'minimum_messages']
 
 class ProfileDetailsSerializer(serializers.ModelSerializer):
-    user = UserShortSerializer(many=False, read_only=True)
+    user = serializers.SerializerMethodField()
     top_group = GroupSerializer(many=False, read_only=True)
     groups = GroupSerializer(many=True, read_only=True)
+
     class Meta:
         model = Profile
         fields = ['user', 'messages_count', 'desc', 'localisation', 
                   'loisirs', 'birthdate',  'type', 'favorite_games', 
                   'zodiac_sign', 'gender', 'website', 'skype', 'signature', 
                   'last_login', 'name_color', 'top_group', 'groups']
+        
+    def get_user(self, obj):
+        """If email is public, include it in the user data."""
+        user_data = UserShortSerializer(obj.user).data
+        if obj.email_is_public:
+            user_data['email'] = obj.user.email
+        return user_data
