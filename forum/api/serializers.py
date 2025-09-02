8c502eefb06b34192ec02f578172d9527ddae5a1
Théo Torregrossa <152283855+theotor83@users.noreply.github.com>
2025-09-02 11:09:01 +0200

    Add group serialization to profile details endpoint
    Introduced GroupSerializer for ForumGroup model and updated ProfileDetailsSerializer to include top_group and groups fields. This will expose group-related information in API responses.

--- forum/api/serializers.py
+++ forum/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Post, Profile
+from ..models import *
 from django.contrib.auth.models import User
 
 class PostDebugSerializer(serializers.ModelSerializer):
@@ -12,10 +12,19 @@ class UserShortSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'date_joined']
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumGroup
+        fields = ['name', 'description', 'color', 'priority', 'is_staff_group', 
+                  'is_messages_group', 'minimum_messages']
+
 class ProfileDetailsSerializer(serializers.ModelSerializer):
     user = UserShortSerializer(many=False, read_only=True)
+    top_group = GroupSerializer(many=False, read_only=True)
+    groups = GroupSerializer(many=True, read_only=True)
     class Meta:
         model = Profile
         fields = ['user', 'messages_count', 'desc', 'localisation', 
                   'loisirs', 'birthdate',  'type', 'favorite_games', 
-                  'zodiac_sign', 'gender', 'website', 'skype', 'signature', 'last_login']
+                  'zodiac_sign', 'gender', 'website', 'skype', 'signature', 
+                  'last_login', 'name_color', 'top_group', 'groups']
