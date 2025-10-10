f5f9f0689002a72adc2b4b7ae7bb23e0b2eb3bef
Théo Torregrossa <152283855+theotor83@users.noreply.github.com>
2025-10-10 16:03:50 +0200

    Add notifications for new topic creation
    Introduces signal handler to notify users watching a subforum or category when a new topic is created. Also refines post notification logic to only notify on replies, not the initial post in a topic.

--- forum/signals.py
+++ forum/signals.py
@@ -8,11 +8,11 @@ from utf.utils import cprint
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @receiver(post_save, sender=Post)
-def send_watched_topic_notification(sender, instance, created, **kwargs):
+def send_watched_topic_notification_post(sender, instance, created, **kwargs):
     """
     After a post is created, notify all users watching the topic/subforum/category.
     """
-    if created:
+    if created and instance.get_relative_id != 1:  # Only notify for replies, not the first post in a topic
         topic = instance.topic
 
         watchers_id_list = []
@@ -40,6 +40,43 @@ def send_watched_topic_notification(sender, instance, created, **kwargs):
             notification = {
                 'message': f"{instance.author.username} a posté une réponse sur \"{topic.get_short_title()}\".",
                 'text_preview': instance.get_short_text(100),
+                'post_url': instance.get_absolute_url,
+                'author_username': instance.author.username,
+                'topic_full_title': topic.title,
+            }
+            redis_client.publish(channel_name, json.dumps(notification))
+            cprint(f"Published notification for User {user_id} to channel {channel_name}")
+            already_notified_user_ids.append(user_id)
+
+@receiver(post_save, sender=Topic)
+def send_watched_topic_notification_topic(sender, instance, created, **kwargs):
+    """
+    After a topic is created, notify all users watching the subforum/category.
+    """
+    if created:
+        topic = instance
+
+        watchers_id_list = []
+        for user in topic.category.watchers.all():
+            watchers_id_list.append(user.id)
+
+        parent = topic.parent
+        while parent:
+            for user in parent.watchers.all():
+                if user.id not in watchers_id_list:
+                    watchers_id_list.append(user.id)
+            parent = parent.parent
+        
+        already_notified_user_ids = [instance.author.id]
+
+        for user_id in watchers_id_list:
+            if user_id in already_notified_user_ids:
+                continue
+            channel_name = f"user_notifications_{user_id}"
+
+            notification = {
+                'message': f"{instance.author.username} a créé un nouveau sujet \"{topic.get_short_title()}\".",
+                'text_preview': f"Cliquez ici pour voir le sujet.",
                 'post_url': instance.get_absolute_url,
                 'author_username': instance.author.username,
                 'topic_full_title': topic.title,
